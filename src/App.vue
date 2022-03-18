b141d9c4d8c0f7dfba3bf87d111921db43231e7e
blackbadgerlabs <blackbadgerlabs@gmail.com>
2022-03-18 09:43:08 +0000

    feat: Add the Basic Setup steps for our app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome!" />
+  <HelloWorld :header="header" :steps="steps" />
 </template>
 
 <script>
 import HelloWorldVue from "./components/HelloWorld.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      header: "How we made this app",
+      steps: [
+        {
+          name: "Get a Github Account",
+          description: "Register at Github",
+          link: "https://github.com/",
+          done: false,
+        },
+        {
+          name: "Create Your Sandbox",
+          description: "Register at codesandbox using your github account",
+          link: "https://codesandbox.io/",
+        },
+        {
+          name: "Select your template",
+          description: "select create from a template, and then select Vue 3",
+        },
+        {
+          name: "Link Your Sandbox to Github",
+          description:
+            "Press the button that looks like the github cat logo and select a name for your repository and create it",
+          note: "if the creation fails use the steps here to fix it",
+          "note-link":
+            "https://github.com/codesandbox/codesandbox-client/issues/5926",
+        },
+        {
+          name: "Check your new Code Repo",
+          description: "You will now have a new code repo like this one!",
+          link: "https://github.com/blackbadgerlabs/vueexample",
+        },
+        {
+          name: "Deploy your site!",
+          description:
+            "From Codesandbox use the rocket icon to deploy to netlify, and select log in with Github",
+          note:
+            "You can then later log into Netlify and link the repo so you can deploy your updates",
+        },
+      ],
+    };
+  },
   components: {
     HelloWorld: HelloWorldVue,
   },
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,17 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <h1>{{ header }}</h1>
+    <div v-for="(step, index) in steps" :key="step.name">
+      <h2>Step {{ index + 1 }} {{ step.name }}</h2>
+      <a :href="step.link" target="_blank">
+        <p>
+          {{ step.description }}
+        </p>
+      </a>
+      <a v-if="step.note" href="step.note-link" target="_blank">
+        <p class="note">{{ step.note }}</p>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -90,7 +19,8 @@
 export default {
   name: "HelloWorld",
   props: {
-    msg: String,
+    header: String,
+    steps: Array,
   },
 };
 </script>
@@ -109,6 +39,10 @@ li {
   margin: 0 10px;
 }
 a {
+  color: #000000;
+}
+
+.note {
   color: #42b983;
 }
 </style>
