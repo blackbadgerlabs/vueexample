bb68ec264de97092c988dc00f0d19d1d8fc4e742
blackbadgerlabs <blackbadgerlabs@gmail.com>
2022-03-19 17:09:59 +0000

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -24,29 +24,20 @@ export default {
         },
         {
           name: "Create Your Sandbox",
-          description: "Register at codesandbox using your github account",
-          link: "https://codesandbox.io/",
+          description: "Register at stackblitz using your github account",
+          link: "https://stackblitz.com/",
           done: false,
         },
         {
           name: "Select your template",
           description: "select create from a template, and then select Vue 3",
-          image: "select-vue-3-sandbox.png",
           done: false,
-          note:
-            "Don't worry if you see this error. It's a known issue with Codesandbox",
-          note_image: "sandbox-error-message.png",
-          note_link:
-            "https://github.com/codesandbox/codesandbox-client/issues/6194",
         },
         {
           name: "Link Your Sandbox to Github",
           description:
-            "Press the button that looks like the github cat logo and select a name for your repository and create it",
-          note: "if the creation fails use the steps here to fix it",
-          note_link:
-            "https://github.com/codesandbox/codesandbox-client/issues/5926",
-          image: "create-github-repo.png",
+            "Select connect repository and create a new Github repository in the menu that comes up",
+          image: "stackblitz-create-new-repo.png",
           done: false,
         },
         {
@@ -56,16 +47,16 @@ export default {
           done: false,
         },
         {
-          name: "Deploy your site!",
+          name: "Link the repo from Netlify",
           description:
-            "From Codesandbox use the rocket icon to deploy to netlify, and select log in with Github",
-          image: "deploy-to-netlify.png",
+            "Log into Netlify and select add new site and then Import an existing project from a Git repository and choose the Git Repository you have created.",
+          image: "import-existing-site.png",
           done: false,
         },
         {
-          name: "Link the repo from Netlify",
+          name: "Make sure the repo is linked so that the site updates",
           description:
-            "You can then later log into Netlify and link the repo so you can deploy your updates",
+            "You need to make sure that the repo is linked so that it redeplys every time you make a new commit to the main branch",
           image: "link-repo-to-netlify.png",
           done: false,
         },
